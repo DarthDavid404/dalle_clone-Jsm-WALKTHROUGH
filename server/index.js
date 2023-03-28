d9e6453ee9eeb1e928b7e22c7a781a6e7d141b3d
OPT3D <daveketron@gmail.com>
2023-03-28 10:13:58 -0400

    connected mongodb and added 2 post routes

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 import  express  from "express";
 import * as dotenv from "dotenv";
 import cors from "cors"
-import connectDB from './mongodb'
+import connectDB from './mongodb/connect.js'
+import postRoutes from './routes/postRoutes.js'
+import dalleRoutes from './routes/dalleRoutes.js'
 
 dotenv.config();
 
@@ -9,13 +11,22 @@ const app = express();
 app.use(cors());
 app.use(express.json({limit: "50mb"}))
 
+app.use('/api/v1/post', postRoutes)
+app.use('/api/v1/dalle' , dalle)
+
+
 app.get('/', async (req, res) => {
     res.send('Hello from DALL-E!')
 })
 
 
 const startServer = async () => {
-    app.listen(8080, () => console.log('Server started on port 8080'));
+        try {connectDB(process.env.MONGODB_URL)
+    app.listen(8080, () => console.log('Server started on port 8080'));}
+        catch (error) {
+            console.log(error);
+        }
+        
 }
 
 startServer()
